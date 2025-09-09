f1253272744d53cb52d77b5f9d06bb11481be478
speakeasy01 <erikrodny1@gmail.com>
2025-09-09 23:03:54 +0200

    Add player type damage, player types, healt, damage

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,26 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+var health : float = 100:
+	set(value):
+		health = value
+		%Health.value = value
+	
 
 
 func _physics_process(delta):
 	velocity = Input.get_vector("left","right","up","down") * SPEED
 	move_and_collide(velocity * delta)
+
+func take_damage(amount):
+	health -= amount
+	print(amount)
+
+
+func _on_self_damage_body_entered(body: Node2D) -> void:
+	take_damage(body.damage)
+
+
+func _on_timer_timeout() -> void:
+	%Collision.set_deferred("disabled", true)
+	%Collision.set_deferred("disabled", false)
