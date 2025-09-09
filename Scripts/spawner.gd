91f9520818c110c771d63cec9c04d6e04bc89169
speakeasy01 <erikrodny1@gmail.com>
2025-09-09 22:48:02 +0200

    Add enemy recources

--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -5,23 +5,26 @@ extends Node2D
 
 var distance : float = 400
 
+@export var enemy_types : Array[Enemy]
+
 var minute : int:
 	set(value):
 		minute = value
-		%Minute.text = str(value)
+		$HBoxContainer/Minute.text = str(value)
 
-var second : int:
+var _second : int:
 	set(value):
-		second = value
-		if second >= 10:
-			second -= 10
+		_second = value
+		if _second >= 10:
+			_second -= 1
 			minute += 1
-		$HBoxContainer/Second.text = str(second).lpad(2, '0')
+		$HBoxContainer/Second.text = str(_second).lpad(2, '0')
 
 
 func spawn(pos : Vector2):
 	var enemy_instance = enemy.instantiate()
 	
+	enemy_instance.type = enemy_types[min(minute, enemy_types.size()-1)]
 	enemy_instance.position = pos
 	enemy_instance.player_reference = player
 	
@@ -37,5 +40,5 @@ func amount(number : int = 1):
 
 
 func _on_timer_timeout() -> void:
-	second += 1
-	amount(second % 10)
+	_second += 1
+	amount(_second % 10)
