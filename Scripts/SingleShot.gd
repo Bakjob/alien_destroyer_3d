566bb579759e4d47578992d1ba362f3d3a863fd9
speakeasy01 <erikrodny1@gmail.com>
2025-09-10 10:06:09 +0200

    Add new icon map, new weapon recource, new magic wand weapon

--- Scripts/SingleShot.gd
+++ Scripts/SingleShot.gd
@@ -1,2 +1,18 @@
 extends Weapon
 class_name SingleShot
+
+func shoot(source, target, scene_tree):
+	if target == null:
+		return
+	
+	var projectile = projectile_node.can_instantiate()
+	
+	projectile.position = source.position
+	projectile.damage = damage
+	projectile.speed = speed
+	projectile.direction = (target.postion - source.position).normalized()
+	
+	scene_tree.current_scene.add_child(projectile)
+	
+func activate(source, target, scene_tree):
+	shoot(source, target, scene_tree)
